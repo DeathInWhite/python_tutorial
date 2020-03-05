8080605b3e2966fcce19d891b810215d925b2a9b
Cristobal Salinas <36157648+DeathInWhite@users.noreply.github.com>
2020-03-05 07:15:33 -0600

    Entradas y salidas
    Como configurar entradas y salidas

---
+++ Entradas y salidas/escribir_lineas.py
@@ -0,0 +1,9 @@
+import sys
+
+texto = sys.argv[1]
+vecesRepetidas = int(sys.argv[2])
+
+for item in range(vecesRepetidas):
+    print(texto)
+
+#python escribir_lineas.py hola 6
---
+++ Entradas y salidas/primer_script_creado.py
@@ -0,0 +1,7 @@
+import sys
+
+'''Vamos a recibir argumentos desde donde estamos ejecutando el archivo'''
+
+print("Argumentos pasados")
+print(sys.argv)
+#python primer_script_creado.py "este es una cadena de texto" 5
